880860180bf33f339ca4c6b9887b78d4a19a8f94
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-04-20 14:16:10 -0300

    Add import css

--- src/Button/ButtonAus.js
+++ src/Button/ButtonAus.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import "./style.css";
 
 export default function ButtonAus() {
   const [state, setState] = useState("What is the capital of Australia?");
